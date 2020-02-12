bf4b5ee6c2643f9780bf3230347250bb19bf6f4c
arpibb <bencze.arpad.21@gmail.com>
2020-02-12 22:01:58 +0100

    day-11 finishing up

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -3,34 +3,50 @@ const volume = document.querySelector('[name=volume]')
 const playBackRateSlider = document.querySelector('[name=playbackRate]')
 const skippingButtons = document.querySelectorAll('.player__button.skip')
 const videoPlayer = document.querySelector('.player__video')
-const progressFilled = document.querySelector('.progress__filled')
-
-
-progressFilled.style.width = '40%';
-
+const progressBar = document.querySelector('.progress__filled')
+const progressContainer = document.querySelector('.progress')
+const fullScreenButton = document.querySelector('.fullscreen')
 
 function playPuase(){
   // const method = videoPlayer.paused ? 'play' : 'pause'
   // videoPlayer[method]()
   if(videoPlayer.paused){
-    videoPlayer.play()
-    playButton.innerHTML = "&#10074; &#10074;"
+    videoPlayer.play();
+    playButton.innerHTML = "&#10074; &#10074;";
   }
   else {
-    videoPlayer.pause()
-    playButton.innerHTML = '►'
+    videoPlayer.pause();
+    playButton.innerHTML = '►';
   }
 }
 function handleRanges(){
-  videoPlayer[this.name] = this.value
+  videoPlayer[this.name] = this.value;
 }
 function skipping(){
-  videoPlayer.currentTime += parseInt(this.dataset.skip)
+  videoPlayer.currentTime += parseInt(this.dataset.skip);
+}
+function handleProgress(){
+  const percent = videoPlayer.currentTime/videoPlayer.duration *100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+function scrub(e){
+  videoPlayer.currentTime = videoPlayer.duration * (e.offsetX/progressContainer.offsetWidth);
+}
+function goFullscreen(){
+  videoPlayer.requestFullscreen();
 }
 
 playButton.addEventListener('click',playPuase)
 videoPlayer.addEventListener('click',playPuase)
-volume.addEventListener('mousemove',handleRanges)
-playBackRateSlider.addEventListener('mousemove',handleRanges)
+videoPlayer.addEventListener('timeupdate',handleProgress)
+volume.addEventListener('change',handleRanges)
+playBackRateSlider.addEventListener('change',handleRanges)
 skippingButtons.forEach(skippingButton => skippingButton.addEventListener('click',skipping))
+progressContainer.addEventListener('click',scrub)
+let mousedown = false;
+progressContainer.addEventListener('mousemove', (e)=> mousedown && scrub(e));
+progressContainer.addEventListener('mousedown', ()=> mousedown = true);
+progressContainer.addEventListener('mouseup', ()=> mousedown = false);
+fullScreenButton.addEventListener('click', goFullscreen)
+
 
